548ff1aa7c4d4642d2368cbdc6c88daf91b8eb1e
Giordano Lanna <seu-email-no-github@example.com>
2026-02-14 00:35:32 -0300

    outro commit grande que eu perdi a linha do que tava fazendo

---
+++ frontend/src/pages/raw-material-management.tsx
@@ -0,0 +1,9 @@
+import { ProductContent } from "../components/templates/product-content/product-content"
+
+export function RawMaterialManagement() {
+  return (
+    <div className='w-full'>
+      <ProductContent />
+    </div>
+  )
+}
