b0a13d0cd5c9b0ce18952189f778489532a8341b
Giordano Lanna <giordanoal2003@gmail.com>
2026-02-20 22:15:18 -0300

    feat: - implementado tooltip

--- frontend/src/components/molecules/tooltip/tooltip.tsx
+++ frontend/src/components/molecules/tooltip/tooltip.tsx
@@ -1,14 +1,26 @@
-import { Tooltip } from "radix-ui";
+import * as Tooltip from "@radix-ui/react-tooltip"
+import "./tooltip-style.scss"
 
-export default () => (
-	<Tooltip.Provider>
-		<Tooltip.Root>
-			<Tooltip.Trigger />
-			<Tooltip.Portal>
-				<Tooltip.Content>
-					<Tooltip.Arrow />
-				</Tooltip.Content>
-			</Tooltip.Portal>
-		</Tooltip.Root>
-	</Tooltip.Provider>
-);
+interface Prop {
+  content: string
+  children: React.ReactNode
+}
+
+export function InfoTooltip({content, children}: Prop) {
+  return (
+    <Tooltip.Provider>
+      <Tooltip.Root delayDuration={100}>
+        <Tooltip.Trigger asChild>
+          {children}
+        </Tooltip.Trigger>
+
+        <Tooltip.Portal>
+          <Tooltip.Content className="tooltip" side="right">
+            {content}
+            <Tooltip.Arrow className="tooltip-arrow"/>
+          </Tooltip.Content>
+        </Tooltip.Portal>
+      </Tooltip.Root>
+    </Tooltip.Provider>
+  )
+}
