eebdbb3e4f7de037c551e3f2c44829981cd9728a
Giordano Lanna <giordanoal2003@gmail.com>
2026-02-23 20:10:10 -0300

    fix: update CORS configuration to include production origin

--- backend/src/main/java/com/factory_management/config/CorsConfig.java
+++ backend/src/main/java/com/factory_management/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("./**")
-          .allowedOrigins("http://localhost:5173")
+          .allowedOrigins("https://factory-management-azure.vercel.app/", "http://localhost:5173/")
           .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
           .allowedHeaders("*")
           .allowCredentials(true);
