3fdd12ed5288fe410d3f2082c6aa0731c5aa1d69
Giordano Lanna <giordanoal2003@gmail.com>
2026-02-23 19:15:35 -0300

    feat: update Dockerfile for Maven build process and add .dockerignore

--- backend/SQL/db.sql
+++ backend/SQL/db.sql
@@ -16,16 +16,16 @@ CREATE product_requirements (
   id serial PRIMARY KEY,
   amount INT NOT NULL,
   product_id INT NOT NULL,
-  raw_material_id INT NOT NULL
+  raw_material_id INT NOT NULL,
   CONSTRAINT fk_raw_material
     FOREIGN KEY (raw_material_id)
     REFERENCES raw_material(id)
-    ON DELETE CASCADE
+    ON DELETE CASCADE,
 
   CONSTRAINT fk_product
     FOREIGN KEY (product_id)
     REFERENCES product(id)
-    ON DELETE CASCADE
+    ON DELETE CASCADE,
 
 
   UNIQUE (product_id, raw_material_id)
